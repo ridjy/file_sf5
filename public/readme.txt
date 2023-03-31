b01888dab041790baaeceef2216c8f43f020b974
Eddy <eddy@qdata-mg.com>
2023-03-31 17:42:32 +0300

    configuration du form builder

--- public/readme.txt
+++ public/readme.txt
@@ -1,1 +1,3 @@
 les accès Mysql sont par defaut dans .env : root sans mdp port 3306
+
+- l'import de fichier doit être de type csv ou Xls seulement
--- src/Form/ExcelType.php
+++ src/Form/ExcelType.php
@@ -11,7 +11,31 @@ class ExcelType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+        ->add('fichier', FileType::class, [
+            'label' => 'Import des comptes (fichier excel)',
+            // unmapped means that this field is not associated to any entity property
+            'mapped' => false,
+            // make it optional so you don't have to re-upload the PDF file
+            // every time you edit the Product details
+            'required' => true,
+            // unmapped fields can't define their validation using annotations
+            // in the associated entity, so you can use the PHP constraint classes
+            'constraints' => [
+                new File([
+                    'maxSize' => '1024k',
+                    'mimeTypes' => [
+                        'application/vnd.ms-excel', 
+                        'application/x-csv', 
+                        'application/csv', 
+                        'application/excel', 
+                        'application/vnd.msexcel', 
+                        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                    ],
+                    'mimeTypesMessage' => 'Veuiller importer un fichier excel valide',
+                ])
+            ],
+        ])
+        // ...
         ;
     }
 
